85118c39fd0e01f79910c1f0a85b9f5b5daf064a
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 05:58:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ require "pp"
 def reverse_each_word(some)
  reverse_the_word =some.reverse
 sort_some = reverse_the_word.split(/(,)/)
-pp sort_some.sort.reverse
+result = sort_some.sort.reverse
 
 end
