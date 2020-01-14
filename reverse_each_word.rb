3b2dcfc194a3575baee364c7910660bffba5f653
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 05:42:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,6 @@ require "pp"
 def reverse_each_word(some)
  reverse_the_word = some.reverse
  result = reverse_the_word.to_a
-reverse_the_word.collect {|x| -x.length}
+result.collect {|x| -x.length}
 
 end
