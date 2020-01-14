6d8a42700c426c421cf762bd2c169a1ae91c9b10
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 05:20:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 require "pp"
 
-def reverse_each_word(sentence)
-  sentence_literal_string = sentence.split
+def reverse_each_word(some)
+  split_the_word = some.split
    sentence_literal_string.map {|x| x.split}
 end
