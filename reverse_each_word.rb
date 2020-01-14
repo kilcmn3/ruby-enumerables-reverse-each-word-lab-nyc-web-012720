e7d2fe1686878f90bf9fe415a3c991da7b3c2271
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 05:44:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ require "pp"
 
 def reverse_each_word(some)
  reverse_the_word = Array(some.reverse)
-reverse_the_word.collect {|x| -x.length}
+reverse_the_word.collect {|x| x.sort{|i| -i.length}}
 
 end
