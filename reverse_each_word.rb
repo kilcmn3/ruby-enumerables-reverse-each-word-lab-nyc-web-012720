9b08ec88ef6a448ee6a96582bb354aea246eeb5e
DAVID SHIN <kilcmn3@hotmail.com>
2020-01-14 05:39:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,6 @@ require "pp"
 def reverse_each_word(some)
  split_the_word = some.reverse
  split_the_word.collect do |x|
-  one_string = ""
-  one_string << x.reverse
-  print one_string
+
 end
 end
